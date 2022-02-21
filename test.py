0bc6d44010d56d605c0512f3678be9236416db59
Peter Estrada Ledesma <pestradaledesm8169@sdsu.edu>
2022-02-21 02:36:41 -0800

    Uploaded audio files to the folder. Also tried to get a graph going. Not exactly sure what it is plotting.

--- test.py
+++ test.py
@@ -1,3 +1,34 @@
-print("Hi")
+from __future__ import print_function
+import scipy
+import numpy as np
 
-print("testing for github" + 'julia')
+
+
+
+
+from matplotlib import pyplot as plt
+from scipy.fftpack import fft
+import scipy.io.wavfile as wavfile
+
+#import wave
+#wave_file = wave.open("Penguin.wav")
+#freq = wave_file.getframerate()
+#print(freq)
+
+fs, data = wavfile.read('Penguin.wav')      # load the data
+
+a = data.T[0]                               # this is a two channel soundtrack, I get the first track
+#print(a)
+
+b=[(ele/2**8.)*2-1 for ele in a]            # this is 8-bit track, b is now normalized on [-1,1)
+#print(b)
+
+c = fft(b)                                  # calculate fourier transform (complex numbers list)
+#print(c)
+
+d = len(c)/2                                # you only need half of the fft list (real signal symmetry)
+print(d)
+
+
+plt.plot(abs(c[:(int(d)-1)]), 'r')
+plt.show()
